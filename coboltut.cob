ae7cd0844dee10261a23780cf8b2594dd2fa62f2
ArminAfa <90157472+Armin-AF@users.noreply.github.com>
2023-04-26 13:41:28 +0200

    Declration, Basic operations and if statement

---
+++ coboltut.cob
@@ -0,0 +1,124 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.  coboltut.
+       AUTHOR. Armin.
+       DATE-WRITTEN. April 24 2023
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SPECIAL-NAMES. 
+           CLASS PassingScore IS "A" THRU "C".
+
+       DATA DIVISION.
+       FILE SECTION. 
+       WORKING-STORAGE SECTION. 
+       01 UserName PIC X(20) VALUE "Armin".
+       01 Num1 PIC 9 VALUE ZEROS.
+       01 Num2 PIC 9 VALUE ZEROS.
+       01 Total PIC 99 VALUE 0.
+       01 SSNum.
+           02 SSArea PIC 999.
+           02 SSGroup PIC 99.
+           02 SSSerial PIC 9999.
+       01 ans PIC S99V99 VALUE 0.
+       01 Rem PIC 9V99 VALUE 0.
+
+       01 Age PIC 99 VALUE 0.
+       01 Grade PIC 99 VALUE 0.
+       01 Score PIC X(1) VALUE "B".
+       01 CanVoteFlag PIC 9 VALUE 0.
+           88 CanVote VALUE 1.
+           88 CannotVote VALUE 0.
+       01 TestNumber PIC X.
+           88 IsPrime VALUE "1", "2", "3", "5", "7".
+           88 IsOdd VALUE "1", "3", "5", "7", "9".
+           88 IsEven VALUE "0", "2", "4", "6", "8".
+           88 LessThan5 VALUE "0", "1", "2", "3", "4".
+           88 ANumber VALUE "0" THRU "9".
+
+
+       PROCEDURE DIVISION.
+           DISPLAY "What is your name?".
+           ACCEPT UserName.
+           DISPLAY "Hello " UserName.
+
+           MOVE ZERO TO UserName.
+           DISPLAY UserName.
+           DISPLAY "Enter two numbers to add".
+           ACCEPT Num1.
+           ACCEPT Num2.
+           COMPUTE Total = Num1 + Num2.
+           DISPLAY Num1 " + " Num2 " = " Total.
+           DISPLAY "Enter your social security number".
+           ACCEPT SSNum.
+           DISPLAY "Your Area Number is " SSArea.
+
+           ADD Num1 TO Num2 GIVING ans.
+           DISPLAY "The sum of " Num1 " and " Num2 " is " ans.
+           SUBTRACT Num1 FROM Num2 GIVING ans.
+           DISPLAY "The difference of " Num1 " and " Num2 " is " ans.
+           MULTIPLY Num1 BY Num2 GIVING ans.
+           DISPLAY "The product of " Num1 " and " Num2 " is " ans.
+           DIVIDE Num1 BY Num2 GIVING ans.
+           DISPLAY "The quotient of " Num1 " and " Num2 " is " ans.
+           DIVIDE Num1 BY Num2 GIVING ans REMAINDER Rem.
+           DISPLAY "The remainder of " Num1 " and " Num2 " is " Rem.
+
+           COMPUTE ans = Num1 + Num2.
+           DISPLAY "The sum of " Num1 " and " Num2 " is " ans.
+           COMPUTE ans = Num1 - Num2.
+           DISPLAY "The difference of " Num1 " and " Num2 " is " ans.
+           COMPUTE ans = Num1 * Num2.
+           DISPLAY "The product of " Num1 " and " Num2 " is " ans.
+           COMPUTE ans = Num1 / Num2.
+           DISPLAY "The quotient of " Num1 " and " Num2 " is " ans.
+           COMPUTE ans = Num1 ** 2.
+           DISPLAY "The square of " Num1 " is " ans.
+           COMPUTE ans ROUNDED = 3.0 + 2.005.
+           DISPLAY "The rounded sum of 3.0 and 2.005 is " ans.
+           COMPUTE ans = FUNCTION RANDOM(100).
+           DISPLAY "A random number between 0 and 100 is " ans.
+           COMPUTE ans = FUNCTION ABS(-100).
+           DISPLAY "The absolute value of -100 is " ans.
+           COMPUTE ans = FUNCTION SQRT(100).
+           DISPLAY "The square root of 100 is " ans.
+           COMPUTE ans = FUNCTION LENGTH("Hello").
+           DISPLAY "The length of the string 'Hello' is " ans.
+           COMPUTE ans = FUNCTION NUMVAL("123").
+           DISPLAY "The numeric value of the string '123' is " ans.
+           COMPUTE ans = FUNCTION NUMVAL-C("123").
+           DISPLAY "The numeric value of the string '123' is " ans.
+           COMPUTE ans = FUNCTION ORD("A").
+           DISPLAY "The ordinal value of the character 'A' is " ans.
+           COMPUTE ans = FUNCTION DATE-OF-INTEGER(20230424).
+           DISPLAY "The date of the integer 20230424 is " ans.
+
+           DISPLAY "Enter your age: " WITH NO ADVANCING.
+           ACCEPT Age.
+           IF Age >= 18 THEN
+               DISPLAY "You are old enough to vote."
+           ELSE
+               DISPLAY "You are not old enough to vote."
+           END-IF
+
+           DISPLAY "Enter your grade: " WITH NO ADVANCING.
+           ACCEPT Grade.
+           EVALUATE TRUE
+             WHEN Grade < 60
+                  DISPLAY "You failed."
+             WHEN Grade < 70
+                  DISPLAY "You got a D."
+             WHEN Grade < 80
+                  DISPLAY "You got a C."
+             WHEN Grade < 90
+                  DISPLAY "You got a B."
+             WHEN Grade < 100
+                  DISPLAY "You got an A."
+             WHEN OTHER
+                  DISPLAY "You got a perfect score."
+           END-EVALUATE
+
+
+
+           STOP RUN.
+
+
+
