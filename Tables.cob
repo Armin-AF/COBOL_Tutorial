eac8c7dd7cedfe3da8d1a56ca7b8c397fc23dfad
ArminAfa <90157472+Armin-AF@users.noreply.github.com>
2023-08-16 18:33:45 +0200

    SEARCH

--- Tables.cob
+++ Tables.cob
@@ -7,6 +7,11 @@
        01 SALES-TABLE.
          05 MONTHLY-SALES OCCURS 12 TIMES.
             10 DAILY-SALES OCCURS 31 TIMES PIC 9(5).
+       01 NUM-TABLE.
+         05 NUMS PIC 9 OCCURS 9 TIMES INDEXED BY NUMS-IDX.
+       01 IDX PIC 9 VALUE 1.
+       01 NUM-TO-FIND PIC 9 VALUE 6.
+      
 
        PROCEDURE DIVISION.
            MOVE 'JANUARY' TO MONTH-NAME(1).
@@ -18,6 +23,25 @@
            DISPLAY MONTH-NAME(1) ' ' MONTH-NAME(2).
            DISPLAY MONTH-NAME(1) '/' 1 '=' DAILY-SALES (1, 1).
            DISPLAY MONTH-NAME(2) '/' 1 '=' DAILY-SALES (1, 2).
+           MOVE 1 TO NUMS (1).
+           MOVE 2 TO NUMS (2).
+           MOVE 3 TO NUMS (3).
+           MOVE 4 TO NUMS (4).
+           MOVE 5 TO NUMS (5).
+           MOVE 6 TO NUMS (6).
+           MOVE 7 TO NUMS (7).
+           MOVE 8 TO NUMS (8).
+           MOVE 9 TO NUMS (9).
+
+           SET NUMS-IDX TO 1.
+
+           SEARCH NUMS
+             AT END DISPLAY 'NOT FOUND'
+             WHEN NUMS (NUMS-IDX) = NUM-TO-FIND
+               DISPLAY 'FOUND AT POSITION ' NUMS-IDX 
+           END-SEARCH.
+
+
            STOP RUN.
 
        END PROGRAM Tables.
